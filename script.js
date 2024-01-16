846e7cd447ceee1297b2818ae1fabfc1f956bea7
marc-tan <marctan13@gmail.com>
2024-01-16 14:03:24 -0800

    added functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector("#search").addEventListener("click", getPokemon);
-
+document.querySelector("#genOne").addEventListener("click", randomGenOne);
 function lowerCaseName(string) {
   return string.toLowerCase();
 }
@@ -8,6 +8,41 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function randomGenOne() {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 151)}`)
+    .then((response) => response.json())
+    .then(
+      (data) =>
+        (document.querySelector(".pokemonBox").innerHTML = `
+  <div>
+  <img src="${data.sprites.other["official-artwork"].front_default}" alt="${
+          data.name
+        }" /> 
+</div>
+<div class="pokemonInfo">
+  <h1>Name: ${capitalizeFirstLetter(data.name)}</h1>
+  <p>Type: ${capitalizeFirstLetter(data.types[0].type.name)}</p>
+  <p>Weight: ${data.weight} pounds</p>
+  <ul class="moves">
+      <strong>Moves</strong>
+      <li>${
+        data.moves[Math.floor(Math.random() * data.moves.length)].move.name
+      }</li>
+      <li>${
+        data.moves[Math.floor(Math.random() * data.moves.length)].move.name
+      }</li>
+      <li>${
+        data.moves[Math.floor(Math.random() * data.moves.length)].move.name
+      }</li>
+      <li>${
+        data.moves[Math.floor(Math.random() * data.moves.length)].move.name
+      }</li>
+  </ul>
+</div>
+  `)
+    )
+    .catch((error) => console.log("Cannot generate pokemon"));
+}
 
 function getPokemon(e) {
   const name = document.querySelector("#pokemonName").value;
@@ -27,11 +62,24 @@ function getPokemon(e) {
             <h1>Name: ${capitalizeFirstLetter(data.name)}</h1>
             <p>Type: ${capitalizeFirstLetter(data.types[0].type.name)}</p>
             <p>Weight: ${data.weight} pounds</p>
-            <ul>
-                <li>${data.moves[Math.floor(Math.random()*data.moves.length)].move.name}</li>
-                <li>${data.moves[Math.floor(Math.random()*data.moves.length)].move.name}</li>
-                <li>${data.moves[Math.floor(Math.random()*data.moves.length)].move.name}</li>
-                <li>${data.moves[Math.floor(Math.random()*data.moves.length)].move.name}</li>
+            <ul class="moves">
+            <strong>Moves</strong>
+                <li>${
+                  data.moves[Math.floor(Math.random() * data.moves.length)].move
+                    .name
+                }</li>
+                <li>${
+                  data.moves[Math.floor(Math.random() * data.moves.length)].move
+                    .name
+                }</li>
+                <li>${
+                  data.moves[Math.floor(Math.random() * data.moves.length)].move
+                    .name
+                }</li>
+                <li>${
+                  data.moves[Math.floor(Math.random() * data.moves.length)].move
+                    .name
+                }</li>
             </ul>
         </div>
     `) //"-" does not work so cover in brackets for img src
